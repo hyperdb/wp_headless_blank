3f7643d3a849dfd28386d0434200d504f00da724
hyperdb <hyperdb@gmail.com>
2024-02-09 16:03:12 +0900

    🔨追加 - functions.php - `my_auto_slug`

---
+++ wp_headless_blank/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ */
+if(!function_exists('custom_auto_post_slug'))
+{
+	/**
+	 * my_auto_slug
+	 *
+	 * @return string
+	 */
+	function my_auto_slug($_slug, $_post_id, $_post_status, $_post_type)
+	{
+		return md5(sprintf("%s-%d", strtolower(utf8_uri_encode($_post_type)), $_post_id));
+	}
+	add_filter('wp_unique_post_slug', 'my_auto_slug', 10, 4);
+}
