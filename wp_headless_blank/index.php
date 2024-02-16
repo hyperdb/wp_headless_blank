dfe94d75fc19e06741a7edc7186a0ac8c9964d60
hyperdb <hyperdb@gmail.com>
2024-02-16 19:49:21 +0900

    🔨ダークモードを導入 - `darkmode.js`

--- wp_headless_blank/index.php
+++ wp_headless_blank/index.php
@@ -14,6 +14,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php bloginfo('name'); ?></title>
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+    <script>
+        function addDarkmodeWidget() {
+            new Darkmode().showWidget();
+        }
+        window.addEventListener('load', addDarkmodeWidget);
+    </script>
 </head>
 
 <body>
